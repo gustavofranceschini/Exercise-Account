27b0a59dd48027b62029365933da331d206f022e
Gustavo Franceschini <gustavo.araujo18@fatec.sp.gov.br>
2022-08-20 09:45:37 -0300

    Exercise Account

---
+++ Ex-Account/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Course.Entities;
+using Ex_Account.Entities;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "ALex", 500);
+            Account acc2 = new SavingsAccount(1002, "Alex", 500, 0.01);
+
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+
+
+        }
+    }
+}
